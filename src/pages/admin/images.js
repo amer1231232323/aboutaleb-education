187ed6006123f24968a5386c2ea1ca090a49a4c4
Amer Aboutaleb <amer1231232323@gmail.com>
2026-01-07 06:26:23 -0800

    fix admin pages without default component

---
+++ src/pages/admin/images.js
@@ -0,0 +1,8 @@
+export default function AdminImages() {
+  return (
+    <div className="admin-dashboard">
+      <h1>إدارة الصور</h1>
+      <p>سيتم إضافة التحكم في الصور هنا</p>
+    </div>
+  );
+}
---
+++ src/pages/admin/users.js
@@ -0,0 +1,8 @@
+export default function AdminUsers() {
+  return (
+    <div className="admin-dashboard">
+      <h1>إدارة المستخدمين</h1>
+      <p>سيتم إضافة المستخدمين هنا</p>
+    </div>
+  );
+}
